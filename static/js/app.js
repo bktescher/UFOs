bebc35d0790956ae52e1463fb7e274e6c060bc2d
Brett <bnb@bnb.local>
2022-03-09 20:34:00 -0500

    11.5.2 complete

---
+++ static/js/app.js
@@ -0,0 +1,23 @@
+//import the data from data.js
+const tableData= data;
+
+//Reference the HTML table using d3
+var tbody= d3.select("tbody")
+
+function buildTable(data) {
+    //clear existing data
+    tbody.html("");
+
+//loop through each object and append data to row/cells for each value
+data.forEach((dataRow) => {
+
+    // append a row to the table body
+    let row = tbody.append("tr");
+
+    //Loop through each field in datarow, add each value as table cell(td)
+    Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+    });
+  });
+}
